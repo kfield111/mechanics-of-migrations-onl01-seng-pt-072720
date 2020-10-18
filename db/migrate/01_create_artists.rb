4fc56a74fcd38148060b3b95e81211e6ceb41d31
Kelin Field <kfield111@hotmail.com>
2020-10-18 00:54:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,4 +9,17 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   create_table :artists do |t|
     end
   end
+
+  class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
+
+
 end
